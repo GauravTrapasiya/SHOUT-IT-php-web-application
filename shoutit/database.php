ba2f258ac05c3c870b4d1c11a91ee006415f907e
GauravTrapasiya <67284284+GauravTrapasiya@users.noreply.github.com>
2020-10-31 13:54:25 +0530

    Update database.php

--- shoutit/database.php
+++ shoutit/database.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","","shoutit");
+$con = mysqli_connect("localhost","[database root directory]","[password]","shoutit");
 if(mysqli_connect_errno()){
     echo 'failed to connect to MySQLi: '.$mysqli_connect_error(); 
 }
